73ac6218e65951f8e4e4129700463a795c88d4cf
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-12 23:03:59 -0800

    Making player 1

--- StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -5,16 +5,35 @@ namespace Lab04_TicTacToe
 {
     class Program
     {
+        public static string name;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Tic Tac Toe");
-            Console.WriteLine("                          ");
+            Console.WriteLine("      Welcome to Tic Tac Toe");
+            Console.WriteLine("           X O X O X O               ");
 
-            Console.WriteLine("Care to try your luck?");
+            Console.WriteLine("      Care to try your luck?");
             Console.WriteLine("                          ");
-            Console.WriteLine(" Enter player name... ");
+            Console.WriteLine("         Enter player Ones name... ");
+            Console.WriteLine("                          ");
+            string Player1;
+            Player1 = Console.ReadLine();
+            Console.WriteLine($"hello {Player1} ");
+
+            Console.WriteLine("                          ");
+            Console.WriteLine("         Enter player Two name... ");
             Console.WriteLine("                          ");
             Console.ReadLine();
+
+
+            Player P1 = new Player(); 
+            
+               P1.Name = Player1;
+               P1.Marker = "X";
+               
+
+
+
 
 
 
