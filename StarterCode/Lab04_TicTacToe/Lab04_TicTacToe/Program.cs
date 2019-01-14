665e0954c55e6933c6222d8a177a6bc72bb3ac02
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-13 19:14:23 -0800

    if statement for who has won or if the match resulted in a draw.

--- StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -29,37 +29,39 @@ namespace Lab04_TicTacToe
 
             Console.ReadLine();
 
-
+            // Player 1 object
             Player P1 = new Player(); 
             
                P1.Name = Player1;
                P1.Marker = "X";
                P1.IsTurn = true;
 
+            //Player 2 object
             Player P2 = new Player();
 
                 P2.Name = Player2;
                 P2.Marker = "O";
                 P2.IsTurn = false;
 
+            //creates new gameboard
             Board board = new Board();
 
+            //Creates new game
             Game TicTacToe = new Game(P1, P2);
 
+            //Starts new game
             Player PlayerWinner = TicTacToe.Play();
 
-            
+            if (PlayerWinner == null)
+            {
+                Console.WriteLine("Stalemate!");
+            }
+            else
+            {
+                Console.WriteLine($"{PlayerWinner.Name} is NOT the LOSER!");
+            }
 
-
-
-
-
-
-
-
-
-            
-
+        
         }
     }
 }
