b14045e9e2885f4f5026a38d191373ac6e72bb32
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-21 22:10:20 -0800

    adding players and game to test

--- StarterCode/Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
+++ StarterCode/Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
@@ -1,13 +1,20 @@
 using System;
 using Xunit;
+using Lab04_TicTacToe;
+using Lab04_TicTacToe.Classes;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
+        // test to see if a win is produced 
         [Fact]
         public void Test1()
         {
+            Player P1 = new Player();
+            Player P2 = new Player();
+            Game TicTacToe = new Game(P1, P2);
+
 
         }
     }
