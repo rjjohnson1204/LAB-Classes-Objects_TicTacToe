417f9011d3eb584681e59c002c5064a1ee6d5262
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-21 22:25:34 -0800

    created test 2

--- StarterCode/Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
+++ StarterCode/Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
@@ -9,13 +9,32 @@ namespace XUnitTestProject1
     {
         // test to see if a win is produced 
         [Fact]
-        public void Test1()
+        public void Winner()
         {
             Player P1 = new Player();
             Player P2 = new Player();
             Game TicTacToe = new Game(P1, P2);
 
+            TicTacToe.Board.GameBoard[0, 0] = "X";
+            TicTacToe.Board.GameBoard[0, 1] = "X";
+            TicTacToe.Board.GameBoard[0, 2] = "X";
 
+            Assert.True(TicTacToe.CheckForWinner(TicTacToe.Board));
+        }
+
+        // test to see if players switch between turns
+        [Fact]
+        public void SwitchPlayer()
+        {
+            Player P1 = new Player();
+            P1.IsTurn = true;
+            Player P2 = new Player();
+            P2.IsTurn = false;
+            Game TicTacToe = new Game(P1, P2);
+
+            TicTacToe.SwitchPlayer();
+
+            Assert.True(TicTacToe.PlayerTwo.IsTurn);
         }
     }
 }
